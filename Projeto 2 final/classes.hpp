183347fb352f7b3b64889a6354f036069341250f
Fabricio Enric Smith Freire Silva <37123545+fabricioenric@users.noreply.github.com>
2018-03-21 23:25:48 -0300

    Final mesmo
    Final version.

--- Projeto 2 final/classes.hpp
+++ Projeto 2 final/classes.hpp
@@ -12,9 +12,25 @@ public:
       //static int total;
       int getQtd();
       void setQtd(int q);
-      Sapos(int identificador);
-      int tipo;
+      Sapos(int identificador1);
+      int tipo1;
 
 };
+
+class Frogs{
+
+private:
+
+        double intensidade;
+
+public:
+
+        double getIntensidade();
+        void setIntensidade(double intens);
+        Frogs(int identificador2);
+        int tipo2;
+
+};
+
 
 #endif // CLASSES_HPP_INCLUDED
